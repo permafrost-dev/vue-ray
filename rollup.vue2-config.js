563a751f21087e359e679ebd42e7ce8f00149e3b
Patrick Organ <patrick@permafrost.dev>
2021-02-09 19:52:31 -0500

    standardize config files

--- rollup.vue2-config.js
+++ rollup.vue2-config.js
@@ -1,59 +1,37 @@
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 const options = {
-    sourceMapsEnabled: true,
+    sourceMapsEnabled: false,
     minified: false,
-};
-
-const outputs = {
-    minified: options.minified ? [
-        {
-            file: 'dist/vue2.cjs.min.js',
-            format: 'cjs',
-            plugins: [terser()],
-            sourcemap: options.sourceMapsEnabled,
-            exports: 'auto',
-        },
-        {
-            file: 'dist/vue2.min.mjs',
-            format: 'esm',
-            plugins: [terser()],
-            sourcemap: options.sourceMapsEnabled,
-        },
-    ] : [],
-    unminified: [
-        {
-            file: 'dist/vue2.cjs.js',
-            format: 'cjs',
-            sourcemap: options.sourceMapsEnabled,
-            exports: 'auto',
-            plugins: []
-        },
-        {
-            file: 'dist/vue2.mjs',
-            format: 'esm',
-            sourcemap: options.sourceMapsEnabled,
-            plugins: []
-        },
-    ]
+    version: 2,
 };
 
 export default {
-    input: 'src/RayVue2.js',
+    input: `src/RayVue${options.version}.js`,
     output: [
-        ...outputs.unminified,
-        ...outputs.minified,
+        {
+            file: `dist/vue${options.version}.cjs.js`,
+            format: 'cjs',
+            sourcemap: options.sourceMapsEnabled,
+            exports: 'auto',
+            plugins: [],
+        },
+        {
+            file: `dist/vue${options.version}.mjs`,
+            format: 'esm',
+            sourcemap: options.sourceMapsEnabled,
+            plugins: [],
+        },
     ],
     plugins: [
         replace({
-            __buildDate__: () => (new Date()).toISOString(),
-            __buildVersion__: () => require('./package.json').version,
+            __BUILD_DATE__: () => new Date().toISOString(),
+            __BUILD_VERSION__: () => require('./package.json').version,
         }),
         nodeResolve(),
         commonjs(),
     ],
-    external: ['axios', 'md5', 'pretty-format', 'stacktrace-js', 'xml-formatter', 'uuid'],
+    external: [],
 };
--- rollup.vue3-config.js
+++ rollup.vue3-config.js
@@ -1,62 +1,37 @@
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
 
 const options = {
-    sourceMapsEnabled: true,
+    sourceMapsEnabled: false,
     minified: false,
-};
-
-const outputs = {
-    minified: options.minified ? [
-        {
-            file: 'dist/vue3.cjs.min.js',
-            format: 'cjs',
-            plugins: [terser()],
-            sourcemap: options.sourceMapsEnabled,
-            exports: 'auto',
-        },
-        {
-            file: 'dist/vue3.min.mjs',
-            format: 'esm',
-            plugins: [terser()],
-            sourcemap: options.sourceMapsEnabled,
-        },
-    ] : [],
-    unminified: [
-        {
-            file: 'dist/vue3.cjs.js',
-            format: 'cjs',
-            sourcemap: options.sourceMapsEnabled,
-            exports: 'auto',
-            plugins: []
-        },
-        {
-            file: 'dist/vue3.mjs',
-            format: 'esm',
-            sourcemap: options.sourceMapsEnabled,
-            plugins: []
-        },
-    ]
+    version: 3,
 };
 
 export default {
-    input: 'src/RayVue3.js',
+    input: `src/RayVue${options.version}.js`,
     output: [
-        ...outputs.unminified,
-        ...outputs.minified,
+        {
+            file: `dist/vue${options.version}.cjs.js`,
+            format: 'cjs',
+            sourcemap: options.sourceMapsEnabled,
+            exports: 'auto',
+            plugins: [],
+        },
+        {
+            file: `dist/vue${options.version}.mjs`,
+            format: 'esm',
+            sourcemap: options.sourceMapsEnabled,
+            plugins: [],
+        },
     ],
     plugins: [
-        commonjs(),
         replace({
-            __buildDate__: () => (new Date()).toISOString(),
-            __buildVersion__: () => require('./package.json').version,
+            __BUILD_DATE__: () => new Date().toISOString(),
+            __BUILD_VERSION__: () => require('./package.json').version,
         }),
         nodeResolve(),
         commonjs(),
-        //typescript(),
     ],
-    external: ['axios', 'md5', 'pretty-format', 'stacktrace-js', 'xml-formatter', 'uuid'],
+    external: [],
 };
