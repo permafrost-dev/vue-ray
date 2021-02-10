3c85c31deeb41aa09dc787bd705cd8d0fbeb90a7
Patrick Organ <patrick@permafrost.dev>
2021-02-09 22:21:53 -0500

    code cleanup

--- vue2.js
+++ vue2.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
 const RayPlugin = require('./dist/vue2.cjs');
 
 module.exports = { RayPlugin };
--- vue3.js
+++ vue3.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
 const RayPlugin = require('./dist/vue3.cjs');
 
 module.exports = { RayPlugin };
