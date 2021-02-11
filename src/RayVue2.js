5293505c69e67a6d939340187f2ac8a9649cccfa
Patrick Organ <patrick@permafrost.dev>
2021-02-10 23:54:36 -0500

    add interceptErrors boolean option to toggle display of Vue errors in Ray

--- src/RayVue2.js
+++ src/RayVue2.js
@@ -3,11 +3,20 @@
 
 const { Ray } = require('node-ray/web');
 
+// send Vue error messages to Ray
+const errorInterceptor = (err, vm) => {
+    vm.$ray(err).red();
+};
+
 const RayPlugin = {
     install: function (Vue, options) {
         Vue.prototype.$ray = (...args) => {
             return Ray.create().send(...args);
         };
+
+        if (options.interceptErrors === true) {
+            Vue.config.errorHandler = errorInterceptor;
+        }
 
         Vue.prototype.$rayVersion = '__BUILD_VERSION__';
     },
--- src/RayVue3.js
+++ src/RayVue3.js
@@ -1,10 +1,19 @@
 const { Ray } = require('node-ray/web');
 
+// send Vue error messages to Ray
+const errorInterceptor = (err, vm) => {
+    vm.$ray(err).red();
+};
+
 const RayPlugin = {
     install: (app, options) => {
         app.config.globalProperties.$ray = (...args) => {
             return Ray.create().send(...args);
         };
+
+        if (options.interceptErrors === true) {
+            app.config.errorHandler = errorInterceptor;
+        }
 
         app.config.globalProperties.$rayVersion = '__BUILD_VERSION__';
 
