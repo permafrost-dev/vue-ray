91ce6b9147aecacfc7ec279a5871f665ad29cbe1
Patrick Organ <patrick@permafrost.dev>
2021-02-11 01:11:36 -0500

    add updated tests for testing interceptErrors option

--- tests/vue2.test.js
+++ tests/vue2.test.js
@@ -1,15 +1,81 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 import RayVue2Plugin from './../src/RayVue2';
 
-test('it installs the Vue 2 plugin', () => {
-    class FakeVue2 {
-        // fake vue class
+let ray;
+
+beforeEach(() => {
+    ray = new FakeRay();
+});
+
+class FakeVm {
+    $ray(...args) {
+        return ray.send(...args);
+    }
+}
+
+class FakeRay {
+    payloads = [];
+
+    send(...args) {
+        if (args.length) {
+            this.payloads.push(
+                ...args.map(arg => ({ name: 'log', content: arg, label: null }))
+            );
+        }
+        return this;
     }
 
+    sendCustom(data, label) {
+        this.payloads.push({ name: 'custom', content: data, label });
+        return this;
+    }
+
+    small() {
+        this.payloads.push({ name: 'small', content: 'small', label: null });
+        return this;
+    }
+
+    red() {
+        this.payloads.push({ name: 'color', content: 'red', label: null });
+        return this;
+    }
+}
+
+class FakeVue2 {
+    // fake vue class
+    static config = {
+        errorHandler: null,
+    };
+
+    constructor() {}
+}
+
+test('it installs the Vue 2 plugin', () => {
     RayVue2Plugin.install(FakeVue2, {});
 
     expect(typeof FakeVue2.prototype.$ray).not.toBe('undefined');
     expect(new FakeVue2().$ray().constructor.name).toBe('Ray');
+});
+
+test('it installs the Vue 2 plugin with the interceptErrors option', () => {
+    RayVue2Plugin.install(FakeVue2, { interceptErrors: true });
+    const myVue = new FakeVue2();
+
+    expect(typeof FakeVue2.prototype.$ray).not.toBe('undefined');
+    expect(new FakeVue2().$ray().constructor.name).toBe('Ray');
+    expect(FakeVue2.config.errorHandler).not.toBeNull();
+    expect(typeof FakeVue2.config.errorHandler).toBe('function');
+
+    const fakeVm = new FakeVm();
+    const myRay = fakeVm.$ray();
+
+    FakeVue2.config.errorHandler({ stack: 'test error' }, fakeVm);
+
+    expect(myRay.payloads.length).toBe(3);
+    expect(myRay.payloads[0].content).toContain('test error');
+    expect(myRay.payloads[1].content).toBe('small');
+    expect(myRay.payloads[2].content).toBe('red');
 });
--- tests/vue3.test.js
+++ tests/vue3.test.js
@@ -3,36 +3,76 @@
 
 import RayVue3Plugin from './../src/RayVue3';
 
+let ray, fakeApp;
+
+class FakeVm {
+    $ray(...args) {
+        return ray.send(...args);
+    }
+}
+
+class FakeRay {
+    payloads = [];
+
+    send(...args) {
+        if (args.length) {
+            this.payloads.push(
+                ...args.map(arg => ({ name: 'log', content: arg, label: null }))
+            );
+        }
+        return this;
+    }
+
+    sendCustom(data, label) {
+        this.payloads.push({ name: 'custom', content: data, label });
+        return this;
+    }
+
+    small() {
+        this.payloads.push({ name: 'small', content: 'small', label: null });
+        return this;
+    }
+
+    red() {
+        this.payloads.push({ name: 'color', content: 'red', label: null });
+        return this;
+    }
+}
+
+class FakeVue3 {
+    //
+}
+
+class FakeApp {
+    constructor() {
+        this.providedItems = [];
+        this.config = {
+            globalProperties: {
+                $ray: null,
+            },
+            errorHandler: null,
+        };
+    }
+
+    provide(name, options) {
+        this.providedItems.push(name);
+    }
+
+    getProvided() {
+        return this.providedItems;
+    }
+
+    getLastProvided() {
+        return this.getProvided().slice(0).pop();
+    }
+}
+
+beforeEach(() => {
+    ray = new FakeRay();
+    fakeApp = new FakeApp();
+});
+
 test('it installs the Vue 3 plugin', () => {
-    class FakeVue3 {
-        //
-    }
-
-    class FakeApp {
-        constructor() {
-            this.providedItems = [];
-            this.config = {
-                globalProperties: {
-                    $ray: null,
-                },
-            };
-        }
-
-        provide(name, options) {
-            this.providedItems.push(name);
-        }
-
-        getProvided() {
-            return this.providedItems;
-        }
-
-        getLastProvided() {
-            return this.getProvided().slice(0).pop();
-        }
-    }
-
-    const fakeApp = new FakeApp();
-
     RayVue3Plugin.install(fakeApp, {});
 
     expect(fakeApp.getLastProvided()).toBe('ray');
@@ -45,4 +85,22 @@ test('it installs the Vue 3 plugin', () => {
         expect(typeof testRay).not.toBe('undefined');
         expect(testRay().constructor.name).toBe('Ray');
     }
+});
+
+test('it installs the Vue 3 plugin with the interceptErrors option', () => {
+    RayVue3Plugin.install(fakeApp, { interceptErrors: true });
+
+    expect(fakeApp.getLastProvided()).toBe('ray');
+    expect(fakeApp.getProvided().length).toBe(1);
+    expect(fakeApp.config.globalProperties['$ray']).not.toBe(null);
+    expect(fakeApp.config.errorHandler).not.toBeNull();
+    expect(typeof fakeApp.config.errorHandler).toBe('function');
+
+    const fakeVm = new FakeVm();
+    fakeApp.config.errorHandler({ stack: 'test error' }, fakeVm);
+
+    expect(ray.payloads.length).toBe(3);
+    expect(ray.payloads[0].content).toContain('test error');
+    expect(ray.payloads[1].content).toBe('small');
+    expect(ray.payloads[2].content).toBe('red');
 });
