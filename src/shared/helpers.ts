d3c686d6e584c52f1e78a11bde6e621f152ecbc1
Patrick Organ <patrick@permafrost.dev>
2021-02-12 14:42:49 -0500

    add html encoding helper function

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -4,3 +4,24 @@ import { PackageInfo } from './PackageInfo';
 export const createPackageMetaProperty = obj => {
     obj.$rayMeta = PackageInfo;
 };
+
+export const encodeHtmlEntities = (str: string) => {
+    const escapeChars: Record<string, string> = {
+        '¢': 'cent',
+        '£': 'pound',
+        '¥': 'yen',
+        '€': 'euro',
+        '©': 'copy',
+        '®': 'reg',
+        '<': 'lt',
+        '>': 'gt',
+        '"': 'quot',
+        '&': 'amp',
+        "'": '#39',
+    };
+
+    const chars: string[] = Object.keys(escapeChars);
+    const regex = new RegExp(`[${chars.join('')}]`, 'g');
+
+    return str.replace(regex, m => `&${escapeChars[m]};`);
+};
