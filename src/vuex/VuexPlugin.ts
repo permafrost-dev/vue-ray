b65fe74acd7fb5ab793f9c9614175922eff48fb0
Patrick Organ <patrick@permafrost.dev>
2021-03-02 11:00:36 -0500

    add support for passing options to toggle logging actions/mutations and tracking state

--- src/vuex/VuexPlugin.ts
+++ src/vuex/VuexPlugin.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
+
 import { ray } from '../shared/VueRay';
 
-export let vuexRay: any = null;
+export let vuexStateRay: any = null;
 
 class VuexState {
     public state: any;
@@ -10,17 +12,81 @@ class VuexState {
     }
 }
 
-export const VuexPlugin = (store: any) => {
-    store.subscribe((mutation: any, state: any) => {
-        const tempState = Object.assign({}, state);
-        const copiedState = Object.freeze(tempState);
+export type RayVuexPluginEventType = 'mutation' | 'action';
 
-        if (vuexRay === null) {
-            vuexRay = ray();
+export type RayVuexPluginLogColor = 'blue' | 'gray' | 'green' | 'orange' | 'purple' | 'red' | 'none';
+
+export const isRayVuexPluginNamedColor = (color?: RayVuexPluginLogColor): boolean => {
+    return typeof color !== 'undefined' && color !== 'none';
+};
+
+export interface RayVuexPluginOptions {
+    trackState?: boolean;
+    logMutations?: boolean;
+    logActions?: boolean;
+    loggedMutationColor?: RayVuexPluginLogColor;
+    loggedActionColor?: RayVuexPluginLogColor;
+}
+
+const DefaultVuexPluginOptions: RayVuexPluginOptions = {
+    trackState: false,
+    logMutations: false,
+    logActions: false,
+    loggedMutationColor: 'none',
+    loggedActionColor: 'none',
+};
+
+const decorateEventType = (name: RayVuexPluginEventType, color: string) => {
+    return `<span class="pr-1 text-${color}-600">${name}</span>`;
+};
+
+const sendEventInfoTable = (name: RayVuexPluginEventType, color: string, data: any) => {
+    return ray().table(
+        {
+            'Vuex Event': decorateEventType(name, color) + ' ' + data.type,
+            Payload: data.payload,
+        },
+        'Vuex'
+    );
+};
+
+export const VuexPlugin = (options: RayVuexPluginOptions = DefaultVuexPluginOptions) => {
+    options = Object.assign({}, DefaultVuexPluginOptions, options);
+
+    return (store: any) => {
+        if (options.trackState) {
+            store.subscribe((mutation: any, state: any) => {
+                const tempState = Object.assign({}, state);
+                const copiedState = Object.freeze(tempState);
+
+                if (vuexStateRay === null) {
+                    vuexStateRay = ray();
+                }
+
+                vuexStateRay = vuexStateRay.send(new VuexState(copiedState));
+            });
         }
 
-        vuexRay = vuexRay.send(new VuexState(copiedState));
-    });
+        if (options.logMutations) {
+            store.subscribe((mutation: any, state: any) => {
+                const r = sendEventInfoTable('mutation', 'green', mutation);
+
+                if (isRayVuexPluginNamedColor(options.loggedMutationColor)) {
+                    r.color(options.loggedMutationColor);
+                }
+            });
+        }
+
+        if (options.logActions) {
+            store.subscribeAction((action: any, state: any) => {
+                const r = sendEventInfoTable('action', 'blue', action);
+
+                if (isRayVuexPluginNamedColor(options.loggedActionColor)) {
+                    r.color(options.loggedActionColor);
+                }
+            });
+        }
+    };
 };
 
 export default VuexPlugin;
