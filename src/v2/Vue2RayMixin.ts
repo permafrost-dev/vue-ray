88d9328f9e6fb5c576fad628ddb844507645e5c2
Patrick Organ <patrick@permafrost.dev>
2021-03-11 15:58:03 -0500

    add event intercepting support

--- src/v2/Vue2RayMixin.ts
+++ src/v2/Vue2RayMixin.ts
@@ -2,7 +2,42 @@
 // @ts-nocheck
 import { VueRay as Vue2Ray } from '../shared/VueRay';
 
+const conditionallyDisplayEvent = (eventName: string, options: Record<string, unknown>, rayInstance: any = null) => {
+    if (Vue2Ray.shouldDisplayEvent(eventName)) {
+        rayInstance = rayInstance ?? this.$ray ?? Vue2RayMixin.methods.$ray;
+
+        rayInstance().table([
+            `component ${eventName}: <code>${options?.name ?? 'unknown'}</code>`,
+            `filename: <code>&lt;project root&gt;/${options?.__file ?? 'unknown.js'}</code>`,
+        ]);
+    }
+};
+
 export const Vue2RayMixin = {
+    beforeCreate() {
+        conditionallyDisplayEvent('before-create', this.$options);
+    },
+
+    beforeMount() {
+        conditionallyDisplayEvent('before-mount', this.$options);
+    },
+
+    created() {
+        conditionallyDisplayEvent('created', this.$options);
+    },
+
+    mounted() {
+        conditionallyDisplayEvent('mounted', this.$options);
+    },
+
+    beforeDestroy() {
+        conditionallyDisplayEvent('unmounted', this.$options);
+    },
+
+    updated() {
+        conditionallyDisplayEvent('updated', this.$options);
+    },
+
     methods: {
         $ray(...args) {
             const ray = Vue2Ray.create();
