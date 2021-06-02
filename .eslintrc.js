2a95844653327eacd24dc5bdcb3d920f2c8b3eef
Patrick Organ <patrick@permafrost.dev>
2021-06-02 14:50:38 -0400

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        ecmaVersion: 2020,
         sourceType: 'module', // Allows for the use of imports
     },
     env: {
@@ -10,18 +10,14 @@ module.exports = {
         commonjs: true,
     },
     settings: {},
-    extends: [
-        //'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'eslint:recommended',
-        //'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
+    extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // '@typescript-eslint/explicit-function-return-type': 'off',
-        // '@typescript-eslint/no-explicit-any': 'off',
-        // '@typescript-eslint/explicit-module-boundary-types': 'off',
-        // '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
         // '@typescript-eslint/no-empty-function': 'off',
+        // 'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
     },
 };
