028d514d872346cd29d33f41e01a69c06498c15b
Patrick Organ <patrick@permafrost.dev>
2021-06-02 14:55:43 -0400

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
         // '@typescript-eslint/no-empty-function': 'off',
         // 'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
         indent: ['error', 4, { SwitchCase: 1 }],
