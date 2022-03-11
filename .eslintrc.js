2da80046d1b0f78e229f899b51e1662b6f5b9e18
Patrick Organ <patrick@permafrost.dev>
2022-03-11 03:15:56 -0500

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2020,
-        sourceType: 'module', // Allows for the use of imports
+        sourceType: 'module',
     },
     env: {
         node: true,
@@ -12,13 +12,13 @@ module.exports = {
     settings: {},
     extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
     rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
-        // '@typescript-eslint/no-empty-function': 'off',
-        // 'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
         indent: ['error', 4, { SwitchCase: 1 }],
     },
 };
