b8f100b7faa19a3faf657b7b10a15462fdd45c21
Gennaro Landolfi <code@rinodrummer.dev>
2024-04-28 18:43:38 +0200

    RayPlugin exported via index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { VueRay, ray } from '@/VueRay';
 import { RayMixin } from '@/RayMixin';
+import { RayPlugin } from '@/RayPlugin';
 import { initializeOptions } from '@/InitializeOptions';
 import { raySetup } from '@/setup';
 
-export { VueRay, ray, RayMixin, initializeOptions, raySetup };
+export { VueRay, ray, RayMixin, RayPlugin, initializeOptions, raySetup };
